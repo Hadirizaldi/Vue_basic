d9bd42d146715d7f48be17b5eaeeefa156182088
Hadi Rizaldi R <hadirizaldi.hr@gmail.com>
2024-01-02 10:37:52 +0700

    konfigurasi menyederhanakan project untuk tahapan selanjutnya selesai

--- vue-compositon-api-basics/src/views/HomeView.vue
+++ vue-compositon-api-basics/src/views/HomeView.vue
@@ -1,9 +1,17 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <div class="home">
+      <h1>Home Page</h1>  
+    </div>
   </main>
 </template>
+
+
+<style>
+@media (min-width: 1024px) {
+  .home {
+    display: flex;
+    justify-content: start;
+  }
+}
+</style>
